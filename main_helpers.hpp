0ae301be8b6dc1195edd29a4e7846155bf981d44
BRUHegg <gkasparov505@gmail.com>
2024-07-17 23:02:27 +0300

    Make main cleaner

--- main_helpers.hpp
+++ main_helpers.hpp
@@ -1,12 +1,26 @@
+#pragma once
+
+
+#include "fpln/fpln_sys.hpp"
+#include "fpln/fpl_cmds.hpp"
+#include "displays/ND/nd.hpp"
 #include <iostream>
 #include <memory>
 #include <string>
-#include "fpln/fpln_sys.hpp"
 #include <libnav/geo_utils.hpp>
+#include <libnav/common.hpp>
+#include <libnav/str_utils.hpp>
 
 
 namespace test
 {
+    const std::string CMD_FILE_NM = "cmds.txt";
+    const std::string PREFS_FILE_NM = "prefs.txt";
+
+    const std::string PREFS_EARTH_PATH = "EPATH";
+    const std::string PREFS_APT_DIR = "APTDIR";
+
+
     class Avionics
     {
     public:
@@ -93,15 +107,179 @@ namespace test
     };
 
 
-    //class CMDInterface
-    //{
-    //public:
-    //    CMDInterface()
-    //    {
-    //
-    //    }
-    //
-    //private:
-    //    std::shared_ptr<Avionics> avncs;
-    //};
+    class CMDInterface
+    {
+    public:
+        CMDInterface()
+        {
+            earth_nav_path = "";
+            apt_dat_dir = "";
+
+            pre_exec = {};
+
+            fetch_prefs_data();
+            get_paths_from_user();
+            get_pre_exec_cmds();
+            create_avionics();
+            pre_execute_cmds();
+        }
+
+        void execute_cmd(std::string in_raw)
+        {
+            std::string in_proc = strutils::strip(in_raw, ' ');
+
+            std::vector<std::string> line_split = strutils::str_split(in_proc, ' ');
+            if(line_split.size())
+            {
+                std::string cmd_name = line_split[0];
+                std::vector<std::string> args = std::vector<std::string>(line_split.begin() + 1, 
+                    line_split.end());
+
+                if(test::cmd_map.find(cmd_name) != test::cmd_map.end())
+                {
+                    test::FPLSys *ptr = avncs->fpl_sys.get();
+                    test::cmd_map[cmd_name](ptr, args);
+                }
+                else
+                {
+                    std::cout << "Invalid command name\n";
+                }
+            }
+        }
+
+        void update()
+        {
+            avncs->update();
+            nd_data->update();
+        }
+
+        void main_loop()
+        {
+            while(1)
+            {
+                std::string in_raw;
+                std::cout << ">> ";
+                std::getline(std::cin, in_raw);
+
+                execute_cmd(in_raw);
+                
+                update();
+            }
+        }
+    
+    private:
+        std::shared_ptr<Avionics> avncs;
+        std::shared_ptr<StratosphereAvionics::NDData> nd_data;
+
+        std::string earth_nav_path;
+	    std::string apt_dat_dir;
+
+        std::vector<std::string> pre_exec;
+
+
+        void fetch_prefs_data()
+        {
+            if(libnav::does_file_exist(PREFS_FILE_NM))
+            {
+                std::ifstream file(PREFS_FILE_NM);
+
+                std::string line;
+                while(getline(file, line))
+                {
+                    line = strutils::strip(line);
+                    if(line.size() && line[0] != '#')
+                    {
+                        std::vector<std::string> str_split = strutils::str_split(line, ' ', 1);
+
+                        if(str_split.size() == 2)
+                        {
+                            if(str_split[0] == PREFS_EARTH_PATH)
+                                earth_nav_path = str_split[1];
+                            else if(str_split[0] == PREFS_APT_DIR)
+                                apt_dat_dir = str_split[1];
+                        }
+                    }
+                }
+
+                file.close();
+            }
+        }
+
+        void update_prefs()
+        {
+            std::ofstream out(PREFS_FILE_NM, std::ofstream::out);
+
+            out << PREFS_EARTH_PATH << " " << earth_nav_path << "\n";
+            out << PREFS_APT_DIR << " " << apt_dat_dir << "\n";
+
+            out.close();
+        }
+
+        void get_paths_from_user()
+        {
+            bool write_to_prefs = false;
+
+            if(earth_nav_path == "")
+            {
+                std::cout << "Please enter path to your Resources/default data directory\n";
+                std::getline(std::cin, earth_nav_path);
+
+                write_to_prefs = true;
+            }
+
+            if(apt_dat_dir == "")
+            {
+                std::cout << "Please enter path to the directory containing apt.dat\n";
+                std::getline(std::cin, apt_dat_dir);
+
+                write_to_prefs = true;
+            }
+
+            if(write_to_prefs)
+            {
+                update_prefs();
+            }
+        }
+
+        void get_pre_exec_cmds()
+        {
+            if(libnav::does_file_exist(CMD_FILE_NM))
+            {
+                std::ifstream file(CMD_FILE_NM);
+
+                std::string line;
+                while(getline(file, line))
+                {
+                    line = strutils::strip(line);
+                    if(line.size() && line[0] != '#')
+                        pre_exec.push_back(line);
+                }
+
+                file.close();
+            }
+        }
+
+        void create_avionics()
+        {
+            avncs = std::make_shared<Avionics>(apt_dat_dir+"apt.dat", "777_arpt.dat", 
+                "777_rnw.dat", earth_nav_path+"earth_fix.dat", 
+                earth_nav_path+"earth_nav.dat", 
+                earth_nav_path+"earth_awy.dat", 
+                earth_nav_path+"earth_hold.dat", earth_nav_path+"CIFP");
+
+            nd_data = std::make_shared<StratosphereAvionics::NDData>(avncs->fpl_sys);
+
+            std::cout << "Avionics loaded\n";
+        }
+
+        void pre_execute_cmds()
+        {
+            for(size_t i = 0; i < pre_exec.size(); i++)
+            {
+                execute_cmd(pre_exec[i]);
+
+                update();
+            }
+        }
+    };
 }
